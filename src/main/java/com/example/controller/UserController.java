da1f5c7ded2248d91d3d920cc473975bc89105b4
narendarpaindla <narendarreddypaindla@gmail.com>
2025-03-06 06:01:18 +0000

    commit

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -2,15 +2,21 @@ package com.example.controller;
 
 import java.util.List;
 
+import org.apache.el.stream.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.User;
 import com.example.service.UserService;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/users")
@@ -24,8 +30,20 @@ public class UserController {
         return userService.createUser(user);
     }
 
-    @GetMapping
+    @GetMapping("/listofuser")
     public List<User> getUsers(){
         return userService.getAllUsers();
     }
-}
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable String id,@RequestBody User userDetails){
+        Optional<User> existingUser = userService.getUserByEmail(userDetails.getEmail());
+        if (existingUser.isPresent()) {
+            User user = existingUser.get();
+            user.setName(userDetails.getName());
+            user.setEmail(userDetails.getEmail());
+            User updatedUser = userService.updateUser(user);
+            return ResponseEntity.ok(updatedUser);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    }
